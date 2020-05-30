00e476db13ddc860d0fbfdeec06b4ab4fda06b42
Ahmed Fathi <ahmedfathi680@gmail.com>
2020-05-30 12:27:01 +0200

    Initial Commit

---
+++ CollectionViewLayouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewLayouts
+//
+//  Created by Ahmed Fathi on 5/30/20.
+//  Copyright © 2020 Ahmed Fathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
